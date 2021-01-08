5a6ed31dc7b9ae20c3eb5f16c135ab71889b1bc6
Wade Pate <wadejp8@gmail.com>
2021-01-08 12:26:01 -0700

    Completed js-2 Problems

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+const gitDefinition = 'Git is a distributed version-control system for tracking changes in any set of files, originally designed for coordinating work among programmers cooperating on source code during software development. Its goals include speed, data integrity, and support for distributed, non-linear workflows.'
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+const gitHubDefinition = 'GitHub is a code hosting platform for version control and collaboration. It lets you and others work together on projects from anywhere.'
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
     For the next several problems you will be creating objects containing information about different git 
@@ -34,7 +34,10 @@
 */
 
 //CODE HERE
-
+const init = {
+    description: 'The git init command creates a new Git repository. It can be used to convert an existing, unversioned project to a Git repository or initialize a new, empty repository. Most other Git commands are not available outside of an initialized repository, so this is usually the first command youll run in a new project.',
+    code: 'git init'
+}
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,6 +45,10 @@
 */
 
 //CODE HERE
+const clone = {
+    description: 'git clone is a Git command line utility which is used to target an existing repository and create a clone, or copy of the target repository... Cloning a local or remote repository.',
+    code: 'git clone'
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -50,7 +57,10 @@
 */
 
 //CODE HERE
-
+const status = {
+    description: 'The git status command displays the state of the working directory and the staging area. It lets you see which changes have been staged, which havent, and which files arent being tracked by Git. Status output does not show you any information regarding the committed project history.',
+    code: 'git status'
+}
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -58,7 +68,10 @@
 */
 
 //CODE HERE
-
+const add = {
+    description: 'git add, adds all modified and new (untracked) files in the current directory and all subdirectories to the staging area (a.k.a. the index), thus preparing them to be included in the next git commit . Any files matching the patterns in the . gitignore file will be ignored by git add.',
+    code: 'git add'
+}
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,7 +79,10 @@
 */
 
 //CODE HERE
-
+const commit = {
+    description: 'A commit is an individual change to a file (or set of files). Its like when you save a file, except with Git, every time you save it creates a unique ID (a.k.a. the "SHA" or "hash") that allows you to keep record of what changes were made when and by who.',
+    code: 'git commit -m ""'
+}
 //////////////////PROBLEM 8////////////////////
 /*
     Create an object called 'addRemote' with 'description' and 'code' properties
@@ -74,7 +90,10 @@
 */
 
 //CODE HERE
-
+const addRemote = {
+    description: 'A remote in Git is a common repository that all team members use to exchange their changes. In most cases, such a remote repository is stored on a code hosting service like GitHub or on an internal server. In contrast to a local repository, a remote typically does not provide a file tree of the projects current state.',
+    code: 'git remote add origin'
+}
 //////////////////PROBLEM 9////////////////////
 /*
     Create an object called 'push' with 'description' and 'code' properties 
@@ -82,3 +101,7 @@
 */
 
 //CODE HERE
+const push = {
+    description: 'The git push command is used to upload local repository content to a remote repository. Pushing is how you transfer commits from your local repository to a remote repo. Its the counterpart to git fetch , but whereas fetching imports commits to local branches, pushing exports commits to remote branches.',
+    code: 'git push'
+}
